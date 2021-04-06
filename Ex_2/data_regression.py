8bb0504fa8031219ca2064ca5bb4e1c6c879ebd4
Andrew_myz2001 <979148792@qq.com>
2021-04-06 16:39:18 +0800

    上传了model

--- Ex_2/data_regression.py
+++ Ex_2/data_regression.py
@@ -26,7 +26,6 @@ y_train = torch.sin(x)
 plot_x = torch.squeeze(x.to('cpu')).numpy()
 plot_y_train = torch.squeeze(y.to('cpu')).numpy()
 
-
 # Using GPU to calculate
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 x = x.to(device)
@@ -37,7 +36,6 @@ model = model.to(device)
 # Setting loss & optimizer
 loss_fun = nn.MSELoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=0.001)
-
 
 # Start training
 epochs = 2000
@@ -53,13 +51,10 @@ for i in range(epochs):
         print(f'Epochs : {i}')
         print(f'Loss : {loss}')
         plot_y_pre = (torch.squeeze(y_pre.to('cpu'))).detach().numpy()
-        plt.ion()  # 开启一个画图的窗口
-
-        plt.clf()  # 清空之前的图
-
+        plt.ion()
+        plt.clf()
         plt.scatter(plot_x, plot_y_train)
         plt.plot(plot_x, plot_y_pre)
-        # plt.plot(plot_x, plot_y_pre)
         plt.pause(0.1)
         plt.ioff()
 
@@ -67,5 +62,5 @@ path = 'Ex2_model.pth'
 torch.save(model.state_dict(), path)
 out = model(x)
 
-# todo matplotlib 的动态制图
-# 这里面有一个是不变的，就是一开始制作的x,y 应该是以曲线展示
+# todo matplotlib 制图换个颜色
+
