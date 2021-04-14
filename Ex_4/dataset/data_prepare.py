afc2f3ecbbc6a82aac8e8c3e62958db18b5cd84e
Andrew_myz2001 <979148792@qq.com>
2021-04-14 15:02:42 +0800

    声明新文件

---
+++ Ex_4/dataset/data_prepare.py
@@ -0,0 +1,15 @@
+"""
+Data Pre-processing for Cat and Dog Dataset
+2021/04/14 MYZ
+"""
+import cv2
+import numpy as np
+import os
+
+# Dataset URl
+url = 'https://www.kaggle.com/tongpython/cat-and-dog'
+
+path = 'cat'
+list = os.listdir(path)
+
+
