71bc9eed1c17ed2567e317fb5a4eec64626059cc
Andrew_myz2001 <979148792@qq.com>
2021-04-14 11:07:06 +0800

    实验3 基本代码完成

--- Ex_3/data_classification.py
+++ Ex_3/data_classification.py
@@ -1,8 +1,10 @@
 import torch
 import torch.nn as nn
+import numpy as np
 import matplotlib.pyplot as plt
 
-class DC_net(nn.modules):
+
+class DC_net(nn.Module):
 
     def __init__(self):
         super(DC_net, self).__init__()
@@ -21,14 +23,15 @@ class DC_net(nn.modules):
 
 # Creating dataset
 data = torch.ones(100, 2)
-x0 = torch.normal(2*data, 1)
-x1 = torch.normal(-2*data, 1)
+x0 = torch.normal(2 * data, 1)
+x1 = torch.normal(-2 * data, 1)
 x = torch.cat((x0, x1))
-
+x_print = x
 y0 = torch.zeros(100)
 y1 = torch.ones(100)
-y = torch.cat((y0,y1)).type(dtype=torch.LongTensor)
-
+y = torch.cat((y0, y1)).type(dtype=torch.LongTensor)
+y_print = y
+y_label = []
 # Using GPU to calculate
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 x = x.to(device)
@@ -41,10 +44,20 @@ optimizer = torch.optim.SGD(model.parameters(), lr=0.03)
 loss_fun = nn.CrossEntropyLoss()
 epoches = 100
 # Training
-# for i in range(epoches):
-#     y_pre = model.forward(x)
-#
-#     loss = loss_fun(y_pre, y)
-#     optimizer.zero_grad()
-#     loss.backward()
-#     optimizer.step()
+for i in range(epoches):
+    y_pre = model.forward(x)
+
+    loss = loss_fun(y_pre, y)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    if i == epoches - 1:
+        y_label = y_pre.to('cpu').detach()
+
+y_prediction = (torch.max(y_label, 1)[1]).numpy()
+target_y = y_print.numpy()
+plt.scatter(x_print.numpy()[:, 0], x_print.numpy()[:, 1], c=y_prediction, s=100, cmap='RdYlGn')
+acc = sum(y_prediction == target_y) / 200
+plt.text(1.5, -4, f'Accuracy is {acc}', fontdict={'size': 20, 'color': 'red'})
+
+print('1')
