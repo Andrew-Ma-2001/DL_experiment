838e7f321068453b2277b992f7b6a4748394f8f5
Andrew_myz2001 <979148792@qq.com>
2021-04-19 19:48:40 +0800

    重构代码

--- Ex_4/dataset/data_prepare.py
+++ Ex_4/dataset/data_prepare.py
@@ -15,12 +15,12 @@ img_y = 256
 train_img_num = 140
 test_img_num = 60
 
-
 # Reading Images
 cat_list = os.listdir('cat')
 dog_list = os.listdir('dog')
 cat_img = []
 dog_img = []
+
 
 def preprocess_img(path):
     img = cv2.imread(path)
@@ -28,8 +28,9 @@ def preprocess_img(path):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     return img
 
+
 for i in range(100):
-    cat_name =  'cat.' + str(i) + '.jpg'
+    cat_name = 'cat.' + str(i) + '.jpg'
     cat_path = 'cat/' + cat_name
     dog_name = 'dog.' + str(i) + '.jpg'
     dog_path = 'dog/' + dog_name
@@ -37,13 +38,12 @@ for i in range(100):
         if cat_name in cat_list and dog_name in dog_list:
             cat_img.append(preprocess_img(cat_path))
             dog_img.append(preprocess_img(dog_path))
-    except :
+    except:
         print(f'Check the name of your dataset')
 
 # Creating Dataset
 # 这里就 140x256x256x3
-train_set = np.zeros(train_img_num*img_x*img_y)
-train_set = np.reshape(train_set, (train_img_num, img_x, img_y,3))
-test_set = np.zeros(test_img_num*img_x*img_y)
-test_set = np.reshape(test_set, (test_img_num, img_x,img_y,3))
-
+train_set = np.zeros(train_img_num * img_x * img_y)
+train_set = np.reshape(train_set, (train_img_num, img_x, img_y, 3))
+test_set = np.zeros(test_img_num * img_x * img_y)
+test_set = np.reshape(test_set, (test_img_num, img_x, img_y, 3))
