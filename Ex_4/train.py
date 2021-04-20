9bd423a5d3f8ef6b5d72529ca446f6e80a56c84b
Andrew_myz2001 <979148792@qq.com>
2021-04-21 00:00:33 +0800

    实验4 声明新文件

--- Ex_4/train.py
+++ Ex_4/train.py
@@ -2,7 +2,11 @@
 Using Dogcat Dataset to Train Classification Pytorch Network
 2021/04/20  MYZ
 """
-
+import torch.nn as nn
 from Ex_4.dataset.dataset import Dogcat_Dataset
 from torch.utils.data import DataLoader
 
+class DogCat_Net(nn.modules):
+    def __init__(self):
+        super(DogCat_Net, self).__init__()
+        self.l1 = nn.Conv2d()
