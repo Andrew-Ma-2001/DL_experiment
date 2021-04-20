4fcb4c42f03ff27e73e0185f6fa62f9a164734d2
Andrew_myz2001 <979148792@qq.com>
2021-04-20 11:48:16 +0800

    实验4 自定义数据集,更改类标

--- Ex_4/dataset/dataset.py
+++ Ex_4/dataset/dataset.py
@@ -1,7 +1,9 @@
 import torch
+from torch.utils.data import DataLoader
 from torch.utils.data import Dataset
 from torchvision.io import read_image
 import os
+import matplotlib.pyplot as plt
 
 def file_name(file_dir):
     list = []
@@ -20,7 +22,7 @@ class Dogcat_Dataset(Dataset):
 
 
     def __getitem__(self, item):
-        if self.train:
+        if self.train == True:
             path = file_name(self.train_dir)[item]
             image = read_image(path)
             label = self.find_label(path)
@@ -33,17 +35,45 @@ class Dogcat_Dataset(Dataset):
             sample = {"image": image, "label": label}
             return sample
 
-    # 回归一个dic 里面 image 为 numpy arr label 为 label
+
     def __len__(self):
         if self.train:
             return(len(file_name(self.train_dir)))
         else:
             return(len(file_name(self.test_dir)))
-    # 回归一个长度
 
+    # Here we consider cat as 0 dog as 1
     def find_label(self, name):
         if name.count('dog') != 0:
-            return 'dog'
+            return 1
         if name.count('cat') != 0:
-            return 'cat'
+            return 0
 
+
+
+if __name__ == '__main__':
+    training_data = Dogcat_Dataset(train_dir='train_set', test_dir='test_set')
+    test_data = Dogcat_Dataset(train_dir='train_set', test_dir='test_set', train=False)
+
+
+    train_dataloader = DataLoader(training_data, batch_size=16, shuffle=True)
+    test_dataloader = DataLoader(test_data, batch_size=16, shuffle=True)
+
+
+    sample_idx = torch.randint(len(training_data), size=(1,)).item()
+    img, label = training_data[sample_idx]
+
+
+
+
+
+
+    # Display image and label.
+    # train_features, train_labels = next(iter(train_dataloader))
+    # print(f"Feature batch shape: {train_features.size()}")
+    # print(f"Labels batch shape: {train_labels.size()}")
+    # img = train_features[0].squeeze()
+    # label = train_labels[0]
+    # plt.imshow(img, cmap="gray")
+    # plt.show()
+    # print(f"Label: {label}")
